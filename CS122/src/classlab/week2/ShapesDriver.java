b1f705868d61537f8d8f9ccf5816fa0217ee95cc
Aaliyah <aaliyahhs22@gmail.com>
2022-02-04 15:29:53 -0500

    classlab week2

---
+++ CS122/src/classlab/week2/ShapesDriver.java
@@ -0,0 +1,12 @@
+package classlab.week2;
+
+public class ShapesDriver {
+
+	public static void main(String[] args) {
+		Rectangle r1 = new Rectangle("Rectangle:", 6, 10);
+		Square s1 = new Square("Square:", 30);
+		
+		System.out.println(r1);
+		System.out.println(s1);
+	}
+}
